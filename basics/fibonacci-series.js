c20aed0e97f22c4f6f62805a30cfe748dd6e321d
nawazshareefshaiksfdc <nawazshareefshaiksfdc@gmail.com>
2025-04-24 00:05:54 +0530

    The fibonacci series

---
+++ basics/fibonacci-series.js
@@ -0,0 +1,40 @@
+let data1 = (num) =>{
+    let n1= 0;
+    let n2 = 1;
+    let tot;
+    let myarray =[]
+    for (let i = 0; i < num; i++) {
+        myarray[i] = n1;
+        tot = n1 + n2;
+        n1 = n2;
+        n2 = tot;
+    }
+    console.log(myarray);
+}
+
+let data2 = (num) =>{
+    let n1= 0;
+    let n2 = 1;
+    let tot;
+    for (let i = 0; i < num; i++) {
+        console.log(n1);
+        tot = n1 + n2;
+        n1 = n2;
+        n2 = tot;
+    }
+}
+
+let data3 = (num) =>{
+    let n1= 0;
+    let n2 = 1;
+    let tot;
+    let myobj = {};
+    for (let i = 0; i < num; i++) {
+        myobj[`the value of ${i} is: `] = n1;
+        tot = n1 + n2;
+        n1 = n2;
+        n2 = tot;
+    }
+ console.log(myobj);
+}
+data3(6);
